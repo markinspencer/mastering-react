0214d5475f0fb07b29e10736279ca1c788456ebb
Spencer Markin <markinspencer@gmail.com>
2018-12-13 20:40:42 -0500

    rename getMovieUrl to movieUrl

--- src/services/movie.service.js
+++ src/services/movie.service.js
@@ -1,7 +1,7 @@
 import http from "./http.service";
 import config from "../config.json";
 
-const getMovieUrl = id => {
+const movieUrl = id => {
   return `${config.movieApiEndpoint}/${id}`;
 };
 
@@ -10,16 +10,16 @@ export const getMovies = () => {
 };
 
 export const getMovie = id => {
-  return http.get(getMovieUrl(id));
+  return http.get(movieUrl(id));
 };
 
 export const saveMovie = movie => {
   const { _id: id, ...body } = movie;
-  if (movie._id) return http.put(getMovieUrl(id), body);
+  if (movie._id) return http.put(movieUrl(id), body);
 
   return http.post(config.movieApiEndpoint, body);
 };
 
 export const deleteMovie = id => {
-  return http.delete(getMovieUrl(id));
+  return http.delete(movieUrl(id));
 };
