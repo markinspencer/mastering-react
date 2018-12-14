3ba22dfb264342e32c21a41f8cddb584ccc86cce
Spencer Markin <markinspencer@gmail.com>
2018-12-13 20:36:41 -0500

    refactor movie service

--- src/services/movie.service.js
+++ src/services/movie.service.js
@@ -1,18 +1,25 @@
 import http from "./http.service";
 import config from "../config.json";
 
+const getMovieUrl = id => {
+  return `${config.movieApiEndpoint}/${id}`;
+};
+
 export const getMovies = () => {
   return http.get(config.movieApiEndpoint);
 };
 
 export const getMovie = id => {
-  return http.get(`${config.movieApiEndpoint}/${id}`);
+  return http.get(getMovieUrl(id));
 };
 
 export const saveMovie = movie => {
-  return http.post(config.movieApiEndpoint, movie);
+  const { _id: id, ...body } = movie;
+  if (movie._id) return http.put(getMovieUrl(id), body);
+
+  return http.post(config.movieApiEndpoint, body);
 };
 
 export const deleteMovie = id => {
-  return http.delete(`${config.movieApiEndpoint}/${id}`);
+  return http.delete(getMovieUrl(id));
 };
