d1a0045b5e04371682be8657937e31c381228a80
Spencer Markin <markinspencer@gmail.com>
2018-12-15 18:09:31 -0500

    display user info and logout buttons when a user is logged in

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { Redirect, Route, Switch } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import jwtDecode from "jwt-decode";
 import Movies from "./components/movies";
 import Rentals from "./components/rentals";
 import Customers from "./components/customers";
@@ -8,16 +10,25 @@ import MovieForm from "./components/movieForm";
 import NavBar from "./components/navBar";
 import LoginForm from "./components/loginForm";
 import RegisterForm from "./components/registerForm";
-import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 
 class App extends Component {
+  state = {};
+
+  componentDidMount = () => {
+    try {
+      const jwt = localStorage.getItem("token");
+      const user = jwtDecode(jwt);
+      this.setState({ user });
+    } catch (err) {}
+  };
+
   render() {
     return (
       <React.Fragment>
         <ToastContainer />
-        <NavBar />
+        <NavBar user={this.state.user} />
         <main role="main" className="container">
           <Switch>
             <Route path="/register" component={RegisterForm} />
--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
 
-const NavBar = () => {
+const NavBar = ({ user }) => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <Link to="/" className="navbar-brand">
@@ -29,12 +29,26 @@ const NavBar = () => {
           <NavLink to="/rentals" className="nav-item nav-link">
             Rentals
           </NavLink>
-          <NavLink to="/login" className="nav-item nav-link">
-            Login
-          </NavLink>
-          <NavLink to="/register" className="nav-item nav-link">
-            Register
-          </NavLink>
+          {user && (
+            <React.Fragment>
+              <NavLink to="/profile" className="nav-item nav-link">
+                {user.name}
+              </NavLink>
+              <NavLink to="/logout" className="nav-item nav-link">
+                Logout
+              </NavLink>
+            </React.Fragment>
+          )}
+          {!user && (
+            <React.Fragment>
+              <NavLink to="/login" className="nav-item nav-link">
+                Login
+              </NavLink>
+              <NavLink to="/register" className="nav-item nav-link">
+                Register
+              </NavLink>
+            </React.Fragment>
+          )}
         </div>
       </div>
     </nav>
