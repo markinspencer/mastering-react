69cd6f0b66572bd2145c9a42e98b3070f8c26d21
Spencer Markin <markinspencer@gmail.com>
2018-12-15 18:13:57 -0500

    implement logout component and functionality

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import MovieForm from "./components/movieForm";
 import NavBar from "./components/navBar";
 import LoginForm from "./components/loginForm";
 import RegisterForm from "./components/registerForm";
+import Logout from "./components/common/logout";
 import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 
@@ -33,6 +34,7 @@ class App extends Component {
           <Switch>
             <Route path="/register" component={RegisterForm} />
             <Route path="/login" component={LoginForm} />
+            <Route path="/logout" component={Logout} />
             <Route path="/customers" component={Customers} />
             <Route path="/rentals" component={Rentals} />
             <Route path="/not-found" component={NotFound} />
---
+++ src/components/common/logout.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class Logout extends Component {
+  componentDidMount = () => {
+    localStorage.removeItem("token");
+    window.location = "/";
+  };
+
+  render = () => null;
+}
+
+export default Logout;
